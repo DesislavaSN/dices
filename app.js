82e23265edbfff76e9e00e96ee1c61288bdbf29f
DesislavaSN <65559215+DesislavaSN@users.noreply.github.com>
2021-06-20 14:18:25 +0100

    Add all files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var randomNum1 = Math.floor((Math.random() * 6) + 1);
+var randomNum2 = Math.floor((Math.random() * 6) + 1);
+// console.log(randomNum1);
+// console.log(randomNum2);
+
+var heading = document.querySelector("h1");
+
+var player1 = document.querySelector(".img1");
+var player2 = document.querySelector(".img2");
+
+player1.setAttribute("src", "pics/dice" + randomNum1 + ".png");
+player2.setAttribute("src", "pics/dice" + randomNum2 + ".png");
+
+if (randomNum1 > randomNum2) {
+  heading.innerText = "Player 1 wins!";
+} else if (randomNum1 < randomNum2) {
+  heading.innerText = "Player 2 wins!";
+} else {
+  heading.innerText = "Draw!";
+}
